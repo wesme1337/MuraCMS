43dab29c8437c17d4fe075840874fc7fa1e0ce24
Matt Levine <matt.levine@blueriver.com>
2019-09-16 17:11:56 -0700

    Adding support for markdown htmlEditors

---
+++ admin/src/toast.js
@@ -0,0 +1,5 @@
+require('codemirror/lib/codemirror.css'); // codemirror
+require('tui-editor/dist/tui-editor.css'); // editor ui
+require('tui-editor/dist/tui-editor-contents.css'); // editor content
+require('highlight.js/styles/github.css');
+markdownEditor=require('tui-editor');

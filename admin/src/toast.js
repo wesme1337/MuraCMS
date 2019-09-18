75800d8e16d5c620964581f666527f05cf45c99a
Matt Levine <matt.levine@blueriver.com>
2019-09-17 22:05:23 -0700

    Working on markdown support

--- admin/src/toast.js
+++ admin/src/toast.js
@@ -34,6 +34,112 @@ getMarkdownEditor=function(config){
     editorInstance=new editor(config);
     
     var toolbar = editorInstance.getUI().getToolbar();
+
+     //BEGIN COMPONENT
+    var AddComponent=editorInstance.commandManager.addCommand('markdown',{
+        name: 'AddComponent',
+        exec: function exec(mde, data) {
+
+            var cm = mde.getEditor();
+            var doc = cm.getDoc();
+        
+            var range = mde.getCurrentRange();
+        
+            var from = {
+                line: range.from.line,
+                ch: range.from.ch
+            };
+        
+            var to = {
+                line: range.to.line,
+                ch: range.to.ch
+            };
+        
+            var text = data.text;
+            text = editorInstance.importManager.constructor.decodeURIGraceful(text);
+            text = editorInstance.importManager.constructor.escapeMarkdownCharacters(text);
+        
+            doc.replaceRange(text, from, to);
+        
+            cm.focus();
+        }
+      });
+
+      var AddComponent=editorInstance.commandManager.addCommand('wysiwyg',{
+        name: 'AddComponent',
+        exec: function exec(wwe, data) {
+            
+            var text = data.text;
+            text = editorInstance.importManager.constructor.decodeURIGraceful(text);
+
+            wwe.insertText(text);
+
+            wwe.focus();
+        }
+
+      });
+
+      editorInstance.addCommand(AddComponent)
+
+     toolbar.addButton({
+        name: 'MuraComponemt',
+        className: 'mi-align-justify',
+        event: 'showMuraComponentPopover',
+        tooltip: 'Image',
+        $el: $('<div style="' + buttonStyles + '"><i class="mi-align-justify"></i></div>')
+    }, 13);
+ 
+    editorInstance.eventManager.addEventType('showMuraComponentPopover');
+    
+    var CLASS_IMAGE_URL_INPUT = 'te-image-url-input';
+    var CLASS_ALT_TEXT_INPUT = 'te-alt-text-input';
+    var CLASS_OK_BUTTON = 'te-ok-button';
+    var CLASS_CLOSE_BUTTON = 'te-close-button';
+
+    var i18n=editorInstance.i18n;
+
+    var COMPONENT_POPUP_CONTENT = `
+        <label for="">Select Component</label>
+        <select class="te-select-component"/>
+        <div class="te-button-section">
+            <button type="button" class="btn ${CLASS_OK_BUTTON}">${i18n.get('OK')}</button>
+            <button type="button" class="btn ${CLASS_CLOSE_BUTTON}">${i18n.get('Cancel')}</button>
+        </div>
+    `;
+
+    var componentPopup=editorInstance.getUI().createPopup({
+        header: true,
+        title: 'Insert Component',
+        className: 'te-popup-add-image tui-editor-popup',
+        content: COMPONENT_POPUP_CONTENT,
+        //$target: editorInstance.getUI().getToolbar().$el,
+        modal:true
+    });
+
+    var componentSelectInput =Mura(componentPopup.$el.get(0)).find('.te-select-component');
+
+    componentPopup.on('shown', function(){ componentSelectInput.focus()});
+    componentPopup.on('hidden',function(){ componentSelectInput.val('');});
+
+    componentPopup.on('click .te-close-button', function(){componentPopup.hide()});
+    componentPopup.on('click .te-ok-button', function(){
+        editorInstance.eventManager.emit('command', 'AddComponent', {
+            text:'HELLO2'
+        });
+        componentPopup.hide();
+    });
+   
+    editorInstance.eventManager.listen('closeAllPopup', function() {
+        componentPopup.hide();
+    });
+
+    editorInstance.eventManager.listen('showMuraComponentPopover', function() {
+       
+        editorInstance.eventManager.emit('closeAllPopup');
+        componentPopup.show();
+
+    });
+    //END COMPONENT
     
     //BEGIN IMAGE
 
@@ -56,12 +162,13 @@ getMarkdownEditor=function(config){
 
     var IMAGE_POPUP_CONTENT = `
         <label for="">${i18n.get('Image URL')}</label>
-        <input type="text" class="${CLASS_IMAGE_URL_INPUT}" />
+        <input type="text" name="mdimageurl" class="${CLASS_IMAGE_URL_INPUT}" style="width:75%;float:left;"/>
+        <button type="button" class="btn mura-finder" data-target="mdimageurl" data-completepath="false" style="width:25%;float:right;">Select Image</button><br/>
         <label for="url">${i18n.get('Description')}</label>
         <input type="text" class="${CLASS_ALT_TEXT_INPUT}" />
         <div class="te-button-section">
-            <button type="button" class="${CLASS_OK_BUTTON}">${i18n.get('OK')}</button>
-            <button type="button" class="${CLASS_CLOSE_BUTTON}">${i18n.get('Cancel')}</button>
+            <button type="button" class="btn ${CLASS_OK_BUTTON}">${i18n.get('OK')}</button>
+            <button type="button" class="btn ${CLASS_CLOSE_BUTTON}">${i18n.get('Cancel')}</button>
         </div>
     `;
 
@@ -74,30 +181,36 @@ getMarkdownEditor=function(config){
         modal:true
     });
 
+    var imageEl = imagePopup.$el.get(0);
+    var imageUrlInput = imageEl.querySelector(`.${CLASS_IMAGE_URL_INPUT}`);
+    var imageAltTextInput = imageEl.querySelector(`.${CLASS_ALT_TEXT_INPUT}`);
+
+    imagePopup.on('shown', function(){ imageUrlInput.focus()});
+    imagePopup.on('hidden',function(){ imagePopup.$el.find('input').val('');});
+
+    imagePopup.on('click .te-close-button', function(){imagePopup.hide()});
+    imagePopup.on('click .te-ok-button', function(){
+        editorInstance.eventManager.emit('command', 'AddImage', {
+            imageUrl:imageUrlInput.value,
+            altText: imageAltTextInput.value || 'image'
+        });
+        imagePopup.hide();
+    });
+
     editorInstance.eventManager.listen('closeAllPopup', function() {
         imagePopup.hide();
     });
 
     editorInstance.eventManager.listen('showMuraImagePopover', function() {
-       
+        
+        imageUrlInput.value='';
+        imageAltTextInput.value='';
+
         editorInstance.eventManager.emit('closeAllPopup');
+
         imagePopup.show();
 
-        var  el = imagePopup.$el.get(0);
-        var imageUrlInput = el.querySelector(`.${CLASS_IMAGE_URL_INPUT}`);
-        var altTextInput = el.querySelector(`.${CLASS_ALT_TEXT_INPUT}`);
-
-        imagePopup.on('shown', function(){ imageUrlInput.focus()});
-        imagePopup.on('hidden',function(){ imagePopup.$el.find('input').val('');});
-
-        imagePopup.on('click .te-close-button', function(){imagePopup.hide()});
-        imagePopup.on('click .te-ok-button', function(){
-            editorInstance.eventManager.emit('command', 'AddImage', {
-                imageUrl:imageUrlInput.value,
-                altText: altTextInput.value || 'image'
-            });
-            imagePopup.hide();
-        });
+        setFinders('button[data-target="mdimageurl"]');
 
     });
     //END IMAGE
@@ -116,12 +229,13 @@ getMarkdownEditor=function(config){
     var i18n=editorInstance.i18n;
     var LINK_POPUP_CONTENT = `
         <label for="url">${i18n.get('URL')}</label>
-        <input type="text" class="te-url-input" />
+        <input type="text" name="mdlinkurl" class="te-url-input" style="width:75%;float:left;"/>
+        <button type="button" class="btn mura-finder" data-target="mdlinkurl" data-completepath="false" style="width:25%;float:right;">Select File</button><br/>
         <label for="linkText">${i18n.get('Link text')}</label>
         <input type="text" class="te-link-text-input" />
         <div class="te-button-section">
-            <button type="button" class="te-ok-button">${i18n.get('OK')}</button>
-            <button type="button" style="margin:unset;hieght:unset;:width:unset;" class="te-close-button">${i18n.get('Cancel')}</button>
+            <button type="button" class="btn te-ok-button">${i18n.get('OK')}</button>
+            <button type="button" class="btn te-close-button">${i18n.get('Cancel')}</button>
         </div>
     `;
 
@@ -134,32 +248,37 @@ getMarkdownEditor=function(config){
         modal:true
     });
  
+
+    var linkEl = LinkPopup.$el.get(0);
+    var linkInputText = linkEl.querySelector('.te-link-text-input');
+    var linkInputURL = linkEl.querySelector('.te-url-input');
+ 
+    LinkPopup.on('click .te-close-button', function(){LinkPopup.hide()});
+    LinkPopup.on('click .te-ok-button', function(){
+        editorInstance.eventManager.emit('command', 'AddLink', {
+            linkText:linkInputText.value,
+            url:linkInputURL.value
+        });
+        LinkPopup.hide()
+    });   
     editorInstance.eventManager.listen('showMuraLinkPopover', function() {
+        var linkSelectedText = editorInstance.getSelectedText().trim();
+        
+        linkInputText.value='';
+        linkInputURL.value='';
 
         editorInstance.eventManager.emit('closeAllPopup');
+
         LinkPopup.show();
         
-        var  el = LinkPopup.$el.get(0);
-        var inputText = el.querySelector('.te-link-text-input');
-        var inputURL = el.querySelector('.te-url-input');
-        var selectedText = editorInstance.getSelectedText().trim();
-
-        inputText.value = selectedText;
-
-        if (URL_REGEX.exec(selectedText)) {
-            inputURL.value = selectedText;
-        }
+        setFinders('button[data-target="mdlinkurl"]');
     
-        inputURL.focus();
+        linkInputText.value = linkSelectedText;
+        linkInputURL.focus();
 
-        LinkPopup.on('click .te-close-button', function(){LinkPopup.hide()});
-        LinkPopup.on('click .te-ok-button', function(){
-            editorInstance.eventManager.emit('command', 'AddLink', {
-                linkText:inputText.value,
-                url:inputURL.value
-            });
-            LinkPopup.hide()
-        });   
+        if (URL_REGEX.exec(linkSelectedText)) {
+            linkInputURL.value = linkSelectedText;
+        }
 
     });
 

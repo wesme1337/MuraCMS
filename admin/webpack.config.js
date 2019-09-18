648b7b4aef9aaaad8937df4d1ebb584443bbaa0d
Matt Levine <matt.levine@blueriver.com>
2019-09-18 15:04:49 -0700

    Adding back accidentally removed files

---
+++ admin/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');	
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {	
+    entry: {	
+        jquery:'./src/jquery.js',	
+        modules:'./src/modules.js',	
+        theme:'./src/theme.js',	
+        bootstrap:'./src/bootstrap.js',
+        toast:'./src/toast.js'	
+    },	
+    target: "web",	
+    output: {	
+        path: path.resolve(__dirname, 'dist'),	
+        filename: '[name].bundle.js'	
+    },	
+    externals: {	
+        jquery: 'jQuery',	
+        'mura.js': 'Mura'	
+    },	
+    module: {	
+        rules: [	
+            {	
+                test: /\.css$/i,	
+                use: ['style-loader', 'css-loader'],	
+            },	
+            {	
+                test: /\.(png|jpe?g|gif|svg)$/i,	
+                loader: 'file-loader',	
+                options: {	
+                    outputPath: './images',
+                    publicPath: 'dist/images',
+                    name: '[contenthash].[ext]',	
+                },	
+            }
+        ],	
+      }
+      
+    /*,	
+      plugins: [
+        new CopyPlugin([
+            {from:'./src/_toast/image-editor',to:'./image-editor'} 
+        ]), 
+    */
+   
+    ]
+  }; 

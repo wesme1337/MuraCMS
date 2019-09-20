56d092dbaa4f1d537058af4bb152bbce95dbffcd
Matt Levine <matt.levine@blueriver.com>
2019-09-19 22:56:05 -0700

    Removing unused js

--- admin/src/layoutmanager.js
+++ admin/src/layoutmanager.js
@@ -1,4 +1,15 @@
-require('mobile-drag-drop'); 
 require('mobile-drag-drop/default.css'); 
+
+import {polyfill} from "mobile-drag-drop";
+
+// optional import of scroll behaviour
+import {scrollBehaviourDragImageTranslateOverride} from "mobile-drag-drop/scroll-behaviour";
+
+// options are optional ;)
+polyfill({
+    // use this to make use of the scroll behaviour
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+});
+
 require('mura.js'); 
 require('./_layoutmanager'); 
